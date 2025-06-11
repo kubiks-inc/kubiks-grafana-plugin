45c8b8d03789b20995aa0fbc149db9507b34836c
Alex Holovach <alex.holovach@kubiks.ai>
2025-06-10 22:43:27 -0500

    add schema editor

--- src/components/testIds.ts
+++ src/components/testIds.ts
@@ -4,6 +4,14 @@ export const testIds = {
     apiUrl: 'data-testid ac-api-url',
     submit: 'data-testid ac-submit-form',
   },
+  serviceMapSchemaEditor: {
+    editor: 'data-testid sms-editor',
+    formatButton: 'data-testid sms-format-button',
+    resetButton: 'data-testid sms-reset-button',
+    saveButton: 'data-testid sms-save-button',
+    validationError: 'data-testid sms-validation-error',
+    validationSuccess: 'data-testid sms-validation-success',
+  },
   pageOne: {
     container: 'data-testid pg-one-container',
     navigateToFour: 'data-testid navigate-to-four',
