957b60e7031a76ba8bc62ed8e71cbd62c894c41b
Alex Holovach <alex.holovach@kubiks.ai>
2025-06-12 18:59:58 -0500

    add demo data

--- src/containers/canvas/InfiniteCanvas.tsx
+++ src/containers/canvas/InfiniteCanvas.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import '@xyflow/react/dist/style.css';
 import { ReactFlow, Controls, Background } from '@xyflow/react';
 
-export const InfiniteCanvas = () => {
+export const InfiniteCanvas = ({data}: {data: any}) => {
+    console.log(data);
     return (
         <div style={{ height: '100%' }}>
           <ReactFlow>
