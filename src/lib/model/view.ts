7cbf4331896135142005f886ac79700bf4d0f9b0
Alex Holovach <alex.holovach@kubiks.ai>
2025-06-13 20:41:03 -0500

    layout items

--- src/lib/model/view.ts
+++ src/lib/model/view.ts
@@ -1,58 +1,58 @@
 export interface ViewSource {
-    integrationId: number
-    recordType: string
-    join: ViewSource[]
-    operator?: string
-    condition?: Condition[]
-  }
-  
-  export interface Condition {
-    field: string
-    operator: string
-    value: any
-  }
-  
-  export interface Selector {
-    data: string
-  }
-  
-  export interface LayoutItem {
-    type: string
-    selector?: Selector
-    label?: string
-    value?: { data: number | string | object }
-  }
-  
-  export interface ViewElement {
-    source?: ViewSource
-    component: string
-    color: string
-    layoutId: string
-    parentId: string
-    order: number
-    id: string
-  }
-  
-  export interface LayoutMetadata {
-    layout: LayoutItem[]
-    id: string
-  }
-  
-  export interface ViewDefinition {
-    layouts: LayoutMetadata[]
-    elements: ViewElement[]
-  }
-  
-  export interface View {
-    id: number
-    name: string
-    description: string
-    type: string
-    organizationId: string
-    metadata: ViewDefinition
-    createdAt: string
-    updatedAt: string
-    snapshotsEnabled: boolean
-    layoutType: string
-  }
-  
+  integrationId: number
+  recordType: string
+  join: ViewSource[]
+  operator?: string
+  condition?: Condition[]
+}
+
+export interface Condition {
+  field: string
+  operator: string
+  value: any
+}
+
+export interface Selector {
+  data: string
+}
+
+export interface LayoutItem {
+  type: 'title' | 'parentId' | 'text' | 'tags' | 'keyValue' | 'progress' | 'inversed_progress' | 'blocks' | 'links' | 'icon' | 'status'
+  source?: string // Query reference
+  selector?: Selector
+  label?: string
+  value?: { data: number | string | object }
+}
+
+export interface ViewElement {
+  source?: ViewSource
+  component: string
+  color: string
+  layoutId: string
+  parentId: string
+  order: number
+  id: string
+}
+
+export interface LayoutMetadata {
+  layout: LayoutItem[]
+  id: string
+}
+
+export interface ViewDefinition {
+  layouts: LayoutMetadata[]
+  elements: ViewElement[]
+}
+
+export interface View {
+  id: number
+  name: string
+  description: string
+  type: string
+  organizationId: string
+  metadata: ViewDefinition
+  createdAt: string
+  updatedAt: string
+  snapshotsEnabled: boolean
+  layoutType: string
+}
